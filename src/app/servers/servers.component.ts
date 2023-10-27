de8f3dd52c810f1fb995a9c3b5e11cde0e83e04b
cesare tibaldeschi <cesare.tibaldeschi@gmail.com>
2023-10-27 15:23:54 +0200

    primo commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit{
+allowNewServer=false;
+serverName='Test Server';
+serverCreationStatus="No server was created."
+username="";
+serverCreated=false;
+
+
+  constructor(){
+
+    setTimeout(
+      () =>{
+        this.allowNewServer=true;
+      },
+    2000)
+  }
+
+ngOnInit(): void {
+
+}
+
+onCreateServer(){
+  this.serverCreated=true;
+  this.serverCreationStatus="The server " + this.serverName + " was created.";
+}
+
+onRemoveServer(){
+  this.serverCreationStatus="The server " + this.serverName + " was removed.";
+}
+onUpdateServerName(event:Event){
+  //console.log(event)
+  this.serverName = (<HTMLInputElement>event.target).value;
+  //console.log(this.serverName)
+}
+
+}
